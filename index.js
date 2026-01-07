be4de3fb392b8603ef1158466a347bba8c576f0e
Swayamx07 <patilswayam1907@gmail.com>
2026-01-07 11:02:41 +0530

    Added in-memory task creation and retrieval APIs

--- index.js
+++ index.js
@@ -3,6 +3,32 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+app.use(express.json());
+
+let tasks = [];
+
+app.post("/tasks", (req, res) => {
+    const {title} = req.body;
+
+    if (!title) {
+        return res.status(400).json({message: "Title is required"});
+    }
+
+    const newTask = {
+        id: Date.now(),
+        title: title,
+        completed: false
+    };
+
+    tasks.push(newTask);
+
+    res.status(201).json(newTask)
+})
+
+app.get("/tasks", (req, res) =>{
+    res.json(tasks);
+})
+
 app.get("/health", (req, res) => {
     res.send("Server running");
 });
